438cc416cd93beb6b00429603d27c5081eff2ae9
Deculsion <unknownkiller100@gmail.com>
2020-05-22 16:34:14 +0800

    Renamed classes

---
+++ FixAmmoUseBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AmmoCycle {
+	class FixAmmoUseBehaviour : ModPlayer {
+
+
+		public override bool PreItemCheck() {
+			
+
+			return base.PreItemCheck();
+		}
+
+	}
+}
