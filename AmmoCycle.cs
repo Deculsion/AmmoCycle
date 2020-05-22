d5f28c80b676bd02c96f79940d177253b3dabd63
Deculsion <unknownkiller100@gmail.com>
2020-05-22 16:34:44 +0800

    Added static instance of class

--- AmmoCycle.cs
+++ AmmoCycle.cs
@@ -4,6 +4,7 @@ namespace AmmoCycle
 {
 	public class AmmoCycle : Mod
 	{
+        public static AmmoCycle instance;
 
         internal static ModHotKey TriggerAmmoCycleNext;
         internal static ModHotKey TriggerAmmoCyclePrev;
@@ -18,6 +19,11 @@ namespace AmmoCycle
             TriggerAmmoCycleNext = RegisterHotKey("Cycle Ammo Next", "Q");
             TriggerAmmoCyclePrev = RegisterHotKey("Cycle Ammo Previous", "Mouse3");
             TriggerAmmoFix = RegisterHotKey("Fix Ammo Used", "J");
+        }
+
+        public override void Unload() {
+            instance = null;
+            base.Unload();
         }
     }
 }
